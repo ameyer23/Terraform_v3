6f6e241ed3c4a3534c9a99903d262c3d10f52200
Andrea Meyer Munoz <andreameyer23@gmail.com>
2024-04-07 16:32:10 -0500

    Added workspaces files

--- BackendProject/providers.tf
+++ BackendProject/providers.tf
@@ -1,13 +1,16 @@
 terraform {
     
+    /*
     # Configure the S3 Backend
     backend "s3" {
-    bucket         = "tf-state-bucket-2acbf1332f4513d1"
+    bucket         = "tf-state-bucket-0d10535545a0631b"
     key            = "remote-backend"
     region         = "us-east-1"
-    dynamodb_table = "tf-state-lock-table-2acbf1332f4513d1"
+    dynamodb_table = "tf-state-lock-table-0d10535545a0631b"
     encrypt        = true
     }
+    */
+
     
     required_providers {
     aws = {
@@ -20,7 +23,6 @@ terraform {
     }
   }
 }
-
 
 
 
